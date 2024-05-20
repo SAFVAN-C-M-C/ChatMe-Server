7f42844421e174e75ea8557b0ef8db68bcb809be
SAFVAN CMC <safvancmc3@gmail.com>
2024-05-20 15:45:02 +0530

    changes in cores

--- gateway/src/util/coresOptions.ts
+++ gateway/src/util/coresOptions.ts
@@ -1,6 +1,14 @@
+import { config } from "dotenv";
+config()
 const allowedOrigins = [String(process.env.CLIENT_URL)];
 export const corsOptions = {
-  origin: allowedOrigins,
+  origin: (origin:any, callback:any) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
