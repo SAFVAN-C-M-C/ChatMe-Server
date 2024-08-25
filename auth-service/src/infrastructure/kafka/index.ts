aa3c53075b600256dd7570293cbbebea8311a3d8
SAFVAN CMC <safvancmc3@gmail.com>
2024-08-25 20:04:48 +0530

    Kafka updated in auth service

--- auth-service/src/infrastructure/kafka/index.ts
+++ auth-service/src/infrastructure/kafka/index.ts
@@ -1,10 +1,27 @@
-import { Kafka,Producer,Partitioners,Consumer } from 'kafkajs'
+import { Kafka, Producer, Partitioners, Consumer } from "kafkajs";
+
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const kafka = new Kafka({
-    clientId: 'auth-service',
-    brokers: ["localhost:29092"]
-  })
-
-  export const producer:Producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner });
-  export const consumer:Consumer = kafka.consumer({groupId:"auth-service-kafka-group"})
-
+  clientId: "auth-service",
+  brokers: [process.env.KAFKA_BROKER_URL as string],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_API_KEY as string,
+    password: process.env.KAFKA_API_SECRET as string,
+  },
+});
+//local kafka
+// const kafka = new Kafka({
+//   clientId: 'auth-service',
+//   brokers: ["localhost:29092"]
+// })
+export const producer: Producer = kafka.producer({
+  createPartitioner: Partitioners.LegacyPartitioner,
+});
+export const consumer: Consumer = kafka.consumer({
+  groupId: "auth-service-kafka-group",
+});
